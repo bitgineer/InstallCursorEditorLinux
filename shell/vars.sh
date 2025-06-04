5077038499124ac539abcc13a505f28fec974827
bitgineer <133060368+bitgineer@users.noreply.github.com>
2025-06-04 11:28:37 -0400

    Update vars.sh
    changed the download to the current latest working image compatiable with yeongpin/cursor-free-vip

--- shell/vars.sh
+++ shell/vars.sh
@@ -12,5 +12,5 @@ readonly DOWNLOAD_EMOJI='\U1F4E2'  # Unicode character for a download emoji
 
 # vars
 readonly OUTPUT_DIRECTORY=~/Cursor
-readonly URL_CURSOR_DOWN="https://www.cursor.com/api/download?platform=linux-x64&releaseTrack=stable"
+readonly URL_CURSOR_DOWN="https://downloads.cursor.com/production/0781e811de386a0c5bcb07ceb259df8ff8246a52/linux/x64/Cursor-0.49.6-x86_64.AppImage" #changed the download to the current latest working image compatiable with yeongpin/cursor-free-vip
 readonly EXEC_PATH="/opt/cursor/squashfs-root/AppRun"
